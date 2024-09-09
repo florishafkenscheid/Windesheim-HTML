2405fa736766d7a6955a667d29dd2fc3efe4b0b6
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-09-09 13:33:54 +0200

    Added js functionality for the dropdown menu, fixed its css and added delay information

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ window.onclick = function(event) {
         }
     }
 }
+
+/* Add different text color based on whether the text is "Delayed" or "Cancelled" in the aside div. */
+document.addEventListener("DOMContentLoaded", function() {
+    const statusElements = document.querySelectorAll(".status-p");
+
+    statusElements.forEach(element => {
+        if (element.textContent.includes("Cancelled")) {
+            element.style.color = "red";
+        } else if (element.textContent.includes("Delayed")) {
+            element.style.color = "orange";
+            let mins = Math.floor(Math.random()*10+1);
+            var msg = element.textContent.concat(` +${mins} minute`);
+            if (mins != 1) {
+                msg = msg.concat('s.');
+            } else {
+                msg = msg.concat('.');
+            }
+            element.textContent = msg;
+        }
+    })
+});
